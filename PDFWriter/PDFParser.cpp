25502313692bad9a089f68448b7c48d1078bc625
Diego Pérez Vieites <dperez@gradiant.org>
2019-06-27 17:32:48 +0200

    Returns encoded data if filter is LZW

--- PDFWriter/PDFParser.cpp
+++ PDFWriter/PDFParser.cpp
@@ -1906,6 +1906,7 @@ IByteReader* PDFParser::CreateInputStreamReader(PDFStreamInput* inStream)
 	}
 	return result;
 }
+#include <iostream>
 
 EStatusCodeAndIByteReader PDFParser::CreateFilterForStream(IByteReader* inStream,PDFName* inFilterName,PDFDictionary* inDecodeParams, PDFStreamInput* inPDFStream)
 {
@@ -2006,7 +2007,7 @@ EStatusCodeAndIByteReader PDFParser::CreateFilterForStream(IByteReader* inStream
 
 			result = mDecryptionHelper.CreateDecryptionFilterForStream(inPDFStream, inStream, cryptFilterName->GetValue());
 		}
-        else if (inFilterName->GetValue() == "CCITTFaxDecode" || inFilterName->GetValue() == "RunLengthDecode")
+        else if (inFilterName->GetValue() == "CCITTFaxDecode" || inFilterName->GetValue() == "RunLengthDecode" || inFilterName->GetValue() == "LZWDecode")
         {
             //TODO: decode stream. This return encoded buffer data. The data can be decoded usign libtiff (we need to create a new IByteReader to CCITT image data)
             result = new InputBufferedStream((IByteReaderWithPosition*)inStream);
