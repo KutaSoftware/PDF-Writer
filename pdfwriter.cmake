e71ac340627d726abcf94f15d605724b66a55dac
pdago <pdago@gradiant.org>
2019-08-07 16:52:24 +0200

    Corrected file

--- pdfwriter.cmake
+++ pdfwriter.cmake
@@ -2,15 +2,15 @@ add_library(pdfwriter STATIC IMPORTED)
 set_target_properties(pdfwriter PROPERTIES IMPORTED_LOCATION ${CMAKE_DEPS}/pdfwriter/lib/libPDFWriter.a)
 target_link_libraries(pdfwriter
         INTERFACE
-        ${CMAKE_DEPS}/pdfwriter/lib/libLibJpeg.a
-        ${CMAKE_DEPS}/pdfwriter/lib/libLibAesgm.a
-        ${CMAKE_DEPS}/pdfwriter/lib/libFreeType.a
-        ${CMAKE_DEPS}/pdfwriter/lib/libLibPng.a
-        ${CMAKE_DEPS}/pdfwriter/lib/libLibTiff.a
-        ${CMAKE_DEPS}/pdfwriter/lib/libZlib.a
+        ${CMAKE_CURRENT_LIST_DIR}/lib/libLibJpeg.a
+        ${CMAKE_CURRENT_LIST_DIR}/lib/libLibAesgm.a
+        ${CMAKE_CURRENT_LIST_DIR}/lib/libFreeType.a
+        ${CMAKE_CURRENT_LIST_DIR}/lib/libLibPng.a
+        ${CMAKE_CURRENT_LIST_DIR}/lib/libLibTiff.a
+        ${CMAKE_CURRENT_LIST_DIR}/lib/libZlib.a
         )
 
 target_include_directories(pdfwriter INTERFACE
-        ${CMAKE_DEPS}/pdfwriter/include/PDFWriter
-        ${CMAKE_DEPS}/pdfwriter/include/LibTiff
+        ${CMAKE_CURRENT_LIST_DIR}/include/PDFWriter
+        ${CMAKE_CURRENT_LIST_DIR}/include/LibTiff
         )
