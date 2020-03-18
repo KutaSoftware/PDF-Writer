4477f72049d73b3b4248c0780ae809cf478f53e1
Iván García <igarcia@gradiant.org>
2020-03-18 09:50:38 +0100

    Disable import libJpeg from pdfwriter artifact

--- pdfwriter.cmake
+++ pdfwriter.cmake
@@ -2,7 +2,7 @@ add_library(pdfwriter STATIC IMPORTED)
 set_target_properties(pdfwriter PROPERTIES IMPORTED_LOCATION ${CMAKE_CURRENT_LIST_DIR}/lib/libPDFWriter.a)
 target_link_libraries(pdfwriter
         INTERFACE
-        ${CMAKE_CURRENT_LIST_DIR}/lib/libLibJpeg.a
+        #${CMAKE_CURRENT_LIST_DIR}/lib/libLibJpeg.a
         ${CMAKE_CURRENT_LIST_DIR}/lib/libLibAesgm.a
         ${CMAKE_CURRENT_LIST_DIR}/lib/libFreeType.a
         ${CMAKE_CURRENT_LIST_DIR}/lib/libLibPng.a
